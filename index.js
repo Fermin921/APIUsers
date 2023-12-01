78170e776704949002507fd427e20f33ca700dd5
Fermin921 <ferminmartinez921@gmail.com>
2023-12-01 08:11:31 -0600

    a

--- index.js
+++ index.js
@@ -310,6 +310,6 @@ app.use("/api-docs-json",(req,res)=>{
 
 
 app.listen(PORT,()=>{
-    console.log("Servidor express escuchando en el puerto 8080");
+    console.log("Servidor express escuchando en el puerto"+PORT);
 });
 
